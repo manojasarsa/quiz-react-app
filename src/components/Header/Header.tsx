880ee5d71775a66ad7c102cb99a8fa02d80e539c
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-09 03:33:45 +0530

    style: add color and border to singout page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import "./header.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { ProfileMenu } from "../ProfileMenu/ProfileMenu";
+import { useAuth } from "../../utils/authMethods";
 
 export const Header = () => {
 
@@ -9,7 +10,11 @@ export const Header = () => {
 
       const menuHandler = () => menu ? setMenu(false) : setMenu(true);
 
-      console.log(menu)
+      const { signOutHandler,
+            authState: {
+                userInfo: { token }
+            } 
+        } = useAuth();
 
       return (
             <header className="main_header">
@@ -17,7 +22,7 @@ export const Header = () => {
 
                         <div className="nav_left logo">
                               <Link className="header_logo" to="/">Quizzically</Link>
-                              <p className="tagline">It's Your Turn</p>
+                              <p className="tagline">You Know, You Grow</p>
                         </div>
 
                         <div className="nav_center searchbar">
@@ -34,9 +39,14 @@ export const Header = () => {
 
                               {menu && <ProfileMenu />   }
 
-                              <Link className="btn btn_primary-outlined" to="/signin">
+                              {token ? 
+                                    <Link className="nav_btn" to="/signout">
+                                          <i onClick={signOutHandler} className="fa-solid fa-right-from-bracket logout_btn"></i>
+                                    </Link>
+                              : <Link className="btn btn_primary-outlined" to="/signin">
                                     Login
                               </Link>
+                              }
 
                         </div>
                   </div>
--- src/pages/Auth/Signout.tsx
+++ src/pages/Auth/Signout.tsx
@@ -10,9 +10,9 @@ export const Signout = () => {
                   <main className="logout_container flex flex_align_center flex_justify_center">
                         <div className="logout_box flex flex_col flex_justify_center flex_align_center">
                               <i className="fas fa-circle-check icon fa-2x"></i>
-                              <p className="logout_text">You are signed out now :(</p>
-                              <p className="logout_text">Come back soon!</p>
-                              <Link className="link" to="/signup">Back to Home</Link>
+                              <p className="logout_text">You Came, You Quizzed, You Conquered!</p>
+                              <p className="logout_text">Come back soon, Champion :)</p>
+                              <Link className="link" to="/">BACK TO HOME</Link>
                         </div>
                   </main>
 
