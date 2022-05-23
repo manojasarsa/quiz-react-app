e42a226f720845af483c8b2c11a36e4be3de1b0e
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-23 05:57:05 +0530

    feat: add README.md file

--- src/contexts/gameContext.tsx
+++ src/contexts/gameContext.tsx
@@ -1,8 +1,62 @@
 import { createContext, useReducer } from "react";
 import { GameContextType, ReactChildrenType } from "../types";
-import { gameReducer } from "../reducers/authReducer";
-
 export const GameContext = createContext({} as GameContextType);
+
+const gameReducer = (state, action) => {
+      switch(action) {
+            case "INITIALIZE" :
+                  return { ...state, error: false, loading: true };
+            
+            case "NEXT_QUES" :
+                  return {
+                        ...state,
+                        loading: false,
+                        currentQuesIndex: state.currentQuesIndex + 1
+                  };
+
+            case "PREV_QUES" :
+                  return {
+                        ...state,
+                        loading: false,
+                        currentQuesIndex: state.currentQuesIndex - 1
+                  };
+
+            case "SET_OPTIONS" :
+                  return {
+                        ...state,
+                        loading: false,
+                  };
+
+            case "SET_QUIZ" :
+                  return {
+                        ...state,
+                        loading: false,
+                  };
+
+            case "RESET_QUESTIONS" :
+                  return {
+                        ...state,
+                        loading: false,
+                  };
+
+            case "RESET_QUIZ" :
+                  return {
+                        ...state,
+                        error: false,
+                        loading: false,
+                        questions: [],
+                        currentQuesIndex: 0,
+                        selectedOptions: [],
+                        quizTitle: ""
+                  };
+
+            case "SET_ERROR" : 
+                  return { ...state, loading: false, error: true };
+
+            default: 
+                  return state;
+      }
+}
 
 export const AuthProvider = ({children}: ReactChildrenType) => {
 
