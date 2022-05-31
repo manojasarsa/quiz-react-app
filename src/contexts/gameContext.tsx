34a41f86f3462f9dd4fe7d4ba2f5b4f98fe75c25
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-05-31 19:27:48 +0530

    feat: _redirects file added

--- src/contexts/gameContext.tsx
+++ src/contexts/gameContext.tsx
@@ -3,77 +3,77 @@ import { GameContextType, ReactChildrenType } from "../types";
 export const GameContext = createContext({} as GameContextType);
 
 const gameReducer = (state, action) => {
-      switch(action) {
-            case "INITIALIZE" :
-                  return { ...state, error: false, loading: true };
-            
-            case "NEXT_QUES" :
-                  return {
-                        ...state,
-                        loading: false,
-                        currentQuesIndex: state.currentQuesIndex + 1
-                  };
+    switch (action) {
+        case "INITIALIZE":
+            return { ...state, error: false, loading: true };
 
-            case "PREV_QUES" :
-                  return {
-                        ...state,
-                        loading: false,
-                        currentQuesIndex: state.currentQuesIndex - 1
-                  };
+        case "NEXT_QUES":
+            return {
+                ...state,
+                loading: false,
+                currentQuesIndex: state.currentQuesIndex + 1
+            };
 
-            case "SET_OPTIONS" :
-                  return {
-                        ...state,
-                        loading: false,
-                  };
+        case "PREV_QUES":
+            return {
+                ...state,
+                loading: false,
+                currentQuesIndex: state.currentQuesIndex - 1
+            };
 
-            case "SET_QUIZ" :
-                  return {
-                        ...state,
-                        loading: false,
-                  };
+        case "SET_OPTIONS":
+            return {
+                ...state,
+                loading: false,
+            };
 
-            case "RESET_QUESTIONS" :
-                  return {
-                        ...state,
-                        loading: false,
-                  };
+        case "SET_QUIZ":
+            return {
+                ...state,
+                loading: false,
+            };
 
-            case "RESET_QUIZ" :
-                  return {
-                        ...state,
-                        error: false,
-                        loading: false,
-                        questions: [],
-                        currentQuesIndex: 0,
-                        selectedOptions: [],
-                        quizTitle: ""
-                  };
+        case "RESET_QUESTIONS":
+            return {
+                ...state,
+                loading: false,
+            };
 
-            case "SET_ERROR" : 
-                  return { ...state, loading: false, error: true };
+        case "RESET_QUIZ":
+            return {
+                ...state,
+                error: false,
+                loading: false,
+                questions: [],
+                currentQuesIndex: 0,
+                selectedOptions: [],
+                quizTitle: ""
+            };
 
-            default: 
-                  return state;
-      }
+        case "SET_ERROR":
+            return { ...state, loading: false, error: true };
+
+        default:
+            return state;
+    }
 }
 
-export const AuthProvider = ({children}: ReactChildrenType) => {
+export const AuthProvider = ({ children }: ReactChildrenType) => {
 
-      const initialGameState = {
-            error: false,
-            loading: false,
-            questions: [{}],
-            currentQuesIndex: 0,
-            selectedOptions: [],
-            quizTitle: ""
-      };  
+    const initialGameState = {
+        error: false,
+        loading: false,
+        questions: [{}],
+        currentQuesIndex: 0,
+        selectedOptions: [],
+        quizTitle: ""
+    };
 
-      const [ gameState, gameDispatch ] = useReducer(gameReducer, initialGameState);
+    const [gameState, gameDispatch] = useReducer(gameReducer, initialGameState);
 
-      return (
-            <GameContext.Provider value={{ gameState, gameDispatch }}>
-                  {children}
-            </GameContext.Provider>
-      );
+    return (
+        <GameContext.Provider value={{ gameState, gameDispatch }}>
+            {children}
+        </GameContext.Provider>
+    );
 }
